48876f9e7c89dc97956ddf0a405f5749791e59ad
Pascal <pasi1995@gmail.com>
2014-02-10 12:18:31 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TssExamples::Application.config.secret_key_base = 'be7b1d72b9cf02f54133431e6dd4fa9903c35fa2835fda83fdd6fe8129ef5bad0fe34f70c0e935187fac34a4734eab3f661ff68bccf65ebd5e522a715730cb63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TssExamples::Application.config.session_store :cookie_store, key: '_tss_examples_session'
